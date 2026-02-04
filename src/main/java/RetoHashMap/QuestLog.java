e43c260cd58ac54977714e2eb4667d50816ff222
fernando corral <146652466+fersho101@users.noreply.github.com>
2026-02-03 23:15:26 -0700

    tarea 3 hecho

--- src/main/java/RetoHashMap/QuestLog.java
+++ src/main/java/RetoHashMap/QuestLog.java
@@ -12,15 +12,24 @@ public class QuestLog {
 
     public void addQuest(Quest quest) {
         // Aquí tu código
+        if(quests.isEmpty() || !quests.containsKey(quest.getId())) {
+            quests.put(quest.getId(),quest);
+        }
     }
 
     public Quest getQuest(String questId) {
         // Aquí tu código
+       if(quests.containsKey(questId)) {
+           return quests.get(questId);
+       }
         return null; // Sustituye null por el valor que sea
     }
 
     public void completeQuest(String questId) {
         // Aquí tu código
+        if(quests.containsKey(questId)) {
+            quests.get(questId).setCompleted(true);
+        }
     }
 
     // Este getter no lo vamos a modificar
