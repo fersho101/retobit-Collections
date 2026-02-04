1eaae49c15e783e3070e4061409f131e043a79b1
fernando corral <146652466+fersho101@users.noreply.github.com>
2026-02-03 22:36:58 -0700

    tarea 1 hecho

--- src/main/java/RetoArrayList/Inventory.java
+++ src/main/java/RetoArrayList/Inventory.java
@@ -1,26 +1,45 @@
 package RetoArrayList;
+
 import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Inventory {
     private final ArrayList<Item> items = new ArrayList<>();
 
     public void addItem(Item item) {
         // Aquí tu código
+        items.add(item);
     }
 
     public boolean removeItem(String itemName) {
         // Aquí tu código
-        return false;
+        AtomicBoolean flag = new AtomicBoolean(false);
+        for (Item e : items) {
+            if (e.getName().equals(itemName)) {
+                items.remove(e.getName());
+                flag.set(true);
+            }
+        }
+        return flag.get();
     }
 
     public Item findItem(String itemName) {
         // Aquí tu código
+        for(Item e : items) {
+            if(e.getName().equals(itemName)) {
+                return e;
+            }
+        }
         return null;
     }
 
     public double getTotalWeight() {
         // Aquí tu código
-        return 0.0; // sustituye esto por el valor real
+        double sum = 0;
+        for(Item e: items) {
+            sum += e.getWeight();
+        }
+        return sum; // sustituye esto por el valor real
     }
 
     // Este getter no es necesario modificarlo
